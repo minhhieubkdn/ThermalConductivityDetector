21d90a359424aec6b160c9f9c17a501340726028
minhhieubkdn <hieucas@gmail.com>
2019-03-30 17:33:05 +0700

    no message

--- Code/Katharometer/Katharometer/HotEnd.cpp
+++ Code/Katharometer/Katharometer/HotEnd.cpp
@@ -9,9 +9,15 @@ HotEnd::HotEnd()
 	this->Kp = KP;
 	this->Ki = KI;
 	this->Kd = KD;
-	this->temperature = 0;
+	this->midTemps[2] = {0};
 	this->setPoint = 0;
-	this->PWMValue = 0;
+	this->PWM1 = 0;
+	this->PWM2 = 0;
+	while (!UpdateTemperature) {};
+	this->LeftTempPID.setBangBang(BANGBANG_TEMP);
+	this->LeftTempPID.setTimeStep(PID_TIME_STEP);
+	this->RightTempPID.setBangBang(BANGBANG_TEMP);
+	this->RightTempPID.setTimeStep(PID_TIME_STEP);
 }
 
 HotEnd::HotEnd(uint8_t * thermistorPins, uint8_t * PWMPins)
@@ -23,11 +29,63 @@ HotEnd::HotEnd(uint8_t * thermistorPins, uint8_t * PWMPins)
 	}
 	this->PWMPins[0] = PWMPins[0];
 	this->PWMPins[1] = PWMPins[1];
+	InitIO();
+}
+
+void HotEnd::InitIO()
+{
+	pinMode(this->PWMPins[0], OUTPUT);
+	pinMode(this->PWMPins[1], OUTPUT);
+	pinMode(this->ledPin, OUTPUT);
 }
 
 void HotEnd::SetDesiredTemp(int & temp)
 {
-
+	this->setPoint = temp;
 }
 
+void HotEnd::Execute()
+{
+	static uint32_t delayTime1 = millis();
+	static uint32_t delayTime2 = millis();
+	if (millis() > delayTime1 + 200) 
+	{
+		QuickUpdateTemp();
+		LeftTempPID.run();
+		analogWrite(PWMPins[0], PWM1);
+		digitalWrite(this->ledPin, LeftTempPID.atSetPoint(2));
+		delayTime1 = millis();
+	}
+
+	if (millis() > delayTime2 + 300)
+	{
+		QuickUpdateTemp();
+		RightTempPID.run();
+		analogWrite(PWMPins[1], PWM2);
+		digitalWrite(this->ledPin, RightTempPID.atSetPoint(4));
+		delayTime2 = millis();
+	}
+}
+
+bool HotEnd::UpdateTemperature()
+{
+	static uint32_t lastUpdate = millis();
+	if (millis() > DELAY_UPDATE_TIME + lastUpdate)
+	{
+		this->midTemps[0] = MidLeft.analog2temp();
+		this->midTemps[1] = MidRight.analog2temp();
+		lastUpdate = millis();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void HotEnd::QuickUpdateTemp()
+{
+	this->midTemps[0] = MidLeft.analog2temp();
+	this->midTemps[1] = MidRight.analog2temp();
+}
 
--- Code/Katharometer/Katharometer/HotEnd.h
+++ Code/Katharometer/Katharometer/HotEnd.h
@@ -32,20 +32,45 @@
 #define KD 0
 #endif
 
+#ifndef DELAY_UPDATE_TIME 
+#define DELAY_UPDATE_TIME 1000
+#endif
+
+#ifndef BANGBANG_TEMP
+#define BANGBANG_TEMP 4
+#endif
+
+#ifndef PID_TIME_STEP
+#define PID_TIME_STEP 4000
+#endif
+
+#ifndef TEMP_LED_PIN
+#define TEMP_LED_PIN 12
+#endif
+
 class HotEnd {
 public:
 	HotEnd();
 	HotEnd(uint8_t * thermistorPins, uint8_t * PWMPins);
 	void SetDesiredTemp(int &temp);
-	AutoPID TempPID = AutoPID(&temperature, &setPoint, &PWMValue, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
-	thermistor Themistor[4];
+	void InitIO();
+	bool UpdateTemperature();
+	void Execute();
+
+	AutoPID LeftTempPID = AutoPID(&midTemps[0], &setPoint, &PWM1, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
+	AutoPID RightTempPID = AutoPID(&midTemps[1], &setPoint, &PWM2, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 	double Kp, Ki, Kd;
 	uint8_t ThermistorPins[4] = {A0, A1, A2, A3};
 	uint8_t PWMPins[2] = {10, 11};
+	uint8_t ledPin = TEMP_LED_PIN;
 private:
-	double temperature;
+	void QuickUpdateTemp();
+	thermistor MidLeft = thermistor(ThermistorPins[0], 0);
+	thermistor MidRight = thermistor(ThermistorPins[1], 0);
+	double midTemps[2];
 	double setPoint;
-	double PWMValue;
+	double PWM1;
+	double PWM2;
 };
 
 #endif
